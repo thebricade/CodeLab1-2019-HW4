f2d2597df7782cddb8f63b4a66b6c79245a5fd6c
Bri <bs3864@nyu.edu>
2019-02-25 14:12:50 -0500

    Added GameManager and ClickToScore Script

---
+++ CodeLab-2019-Week4/Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text scoreText;
+    private int score;
+
+    public int Score
+    {
+        get
+        {
+            return score;
+        }
+        
+        set
+        {
+            score = value;
+            scoreText.text = "Score: " + score;
+        }
+    }
+
+    public static GameManager instance; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad((gameObject));
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
